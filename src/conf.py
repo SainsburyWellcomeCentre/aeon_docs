c2c48092e08bf72c4b407a45503b6e3ecd68003e
lochhh <changhuan.lo@ucl.ac.uk>
2025-01-10 11:18:17 +0100

    Reorganise alerts.md
    Draft alerts module doc

--- src/conf.py
+++ src/conf.py
@@ -81,7 +81,7 @@ myst_enable_extensions = [
 ]
 
 # Automatically add anchors to markdown headings
-myst_heading_anchors = 3
+myst_heading_anchors = 4
 
 # Set the Markdown format to myst
 myst_render_markdown_format = "myst"

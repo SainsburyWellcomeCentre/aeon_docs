5155901de24d8a31446b797cc969953cedead4f0
lochhh <changhuan.lo@ucl.ac.uk>
2025-07-11 18:21:33 +0200

    Temporarily skip aeon_ephys from linkcheck

--- src/conf.py
+++ src/conf.py
@@ -156,6 +156,7 @@ linkcheck_ignore = [
     "http://SubjectExpressionBuilder.Name",  # Broken URL
     "https://learn.microsoft.com/dotnet/api/",  # 429 Client Error: Too Many Requests for url
     "https://www.sainsburywellcome.org/",  # Occasional ConnectTimeoutError
+    "https://github.com/SainsburyWellcomeCentre/aeon_ephys",  # Temporarily disabled as repo is private
 ]
 
 # linkcheck will treat redirections from these source URI:canonical URI

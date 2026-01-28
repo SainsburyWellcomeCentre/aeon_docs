e09ba59e769338e3fc73441dacdeebbe20a68f7d
lochhh <changhuan.lo@ucl.ac.uk>
2026-01-28 12:16:30 +0000

    Support processing multiple targets in make.bat

--- make.bat
+++ make.bat
@@ -10,27 +10,6 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=src
 set BUILDDIR=docs
 
-if "%1" == "" goto help
-
-if not "%1" == "clean" (
-	@echo Building API docs...
-	python make_api_doctree.py
-	python make_acquisition_doctree.py
-
-	@echo Copying example notebooks into 'src'...
-	python copy_examples_to_src.py
-
-	@echo Generating People page...
-	python make_people_list.py
-) else (
-	@echo Removing auto-generated files under 'docs' and 'src'...
-	rmdir /S /Q %BUILDDIR%
-    rmdir /S /Q %SOURCEDIR%\reference\api\
-	del /Q %SOURCEDIR%\about\people.md
-	@echo Removing copied example notebooks from 'src'...
-	del /Q src\user\how_to\*_copy.ipynb
-)
-
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
@@ -44,12 +23,35 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+if "%1" == "" goto help
 
-REM Remove CNAME file from output to prevent setting custom domain on GH Pages
-if exist %BUILDDIR%\CNAME del %BUILDDIR%\CNAME
-if exist %BUILDDIR%\dirhtml\CNAME del %BUILDDIR%\dirhtml\CNAME
-if exist %BUILDDIR%\html\CNAME del %BUILDDIR%\html\CNAME
+:process_targets
+if "%1" == "clean" (
+	echo Removing auto-generated files under 'docs' and 'src'...
+	rmdir /S /Q %BUILDDIR%
+    rmdir /S /Q %SOURCEDIR%\reference\api\
+	del /Q %SOURCEDIR%\about\people.md
+	echo Removing copied example notebooks from 'src'...
+	del /Q src\user\how_to\*_copy.ipynb
+) else (
+	echo Building API docs...
+	python make_api_doctree.py
+	python make_acquisition_doctree.py
+	echo Copying example notebooks into 'src'...
+	python copy_examples_to_src.py
+	echo Generating People page...
+	python make_people_list.py
+
+	%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+	
+	REM Remove CNAME file from output to prevent setting custom domain on GH Pages
+	if exist %BUILDDIR%\CNAME del %BUILDDIR%\CNAME
+	if exist %BUILDDIR%\dirhtml\CNAME del %BUILDDIR%\dirhtml\CNAME
+	if exist %BUILDDIR%\html\CNAME del %BUILDDIR%\html\CNAME
+)
+
+shift
+if not "%1" == "" goto process_targets
 
 goto end
 
